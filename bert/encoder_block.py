74a43205e7e3a1716262403fd738042fb933b1c8
YuriiLaba <laba@ucu.edu.ua>
2024-09-06 01:33:58 +0300

    added pre layer normalization and skip connection

--- bert/encoder_block.py
+++ bert/encoder_block.py
@@ -5,10 +5,16 @@ class EncoderBlock(nn.Module):
     def __init__(self, hidden_size=768, num_heads=12):
         super().__init__()
 
+        self.layer_norm_1 = nn.LayerNorm(hidden_size) 
+        self.layer_norm_2 = nn.LayerNorm(hidden_size)
+
         self.multi_head_attention = MultiHeadAttention(hidden_size, num_heads).to("cuda") 
         self.feed_forward = FeedForward(hidden_size).to("cuda")
 
     def forward(self, x, mask):
-        x = self.multi_head_attention(x, mask)
-        x = self.feed_forward(x)
+        
+        hidden_state = self.layer_norm_1(x) * mask 
+
+        x = x + self.multi_head_attention(hidden_state, mask)
+        x = self.feed_forward(self.layer_norm_2(x * mask ))
         return x
