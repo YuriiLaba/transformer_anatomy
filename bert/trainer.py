c7cbe704aef609f32e142bc1a46e21ce561ffa7a
YuriiLaba <laba@ucu.edu.ua>
2024-09-05 14:32:01 +0300

    added neptune logging, added validation option

--- bert/trainer.py
+++ bert/trainer.py
@@ -2,44 +2,68 @@ import torch
 from bert_dataset import BERTDataset
 from encoder import Encoder
 from bert import BERT
+import neptune
 
 import tqdm
 from transformers import AutoTokenizer
 from torch.utils.data import DataLoader
 
 
+
 class Trainer:
     def __init__(self):
         model_ckpt = "bert-base-uncased"
         self.tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-        self.vocab_size = 30_000
+        self.vocab_size = 30_522
         self.hidden_size = 768
         self.max_length = 128
         self.num_heads = 12
         self.device = 'cpu'
+        
+        self.run = neptune.init_run(
+            project="laba/bert-training",
+            api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIwM2RmMzQyOS01ZWJkLTRmOGMtYTIxMy1mYmE4NzVjNDJhZWYifQ==",
+        ) 
 
         self.model = BERT()
         self.criterion = torch.nn.NLLLoss(ignore_index=-100)
 
         self.train_data = BERTDataset(tokenizer=self.tokenizer, path_to_dataset="datasets/dataset.pkl", max_length=self.max_length)
-        self.train_loader = DataLoader(self.train_data, batch_size=1, shuffle=False, pin_memory=True)
+        self.train_loader = DataLoader(self.train_data, batch_size=5, shuffle=False, pin_memory=True)
+    
+    def stop_logging(self):
+        self.run.stop()
 
     def _train_step(self, batch):
         next_sent_output, mask_lm_output = self.model.forward(batch)
 
-        next_loss = self.criterion(next_sent_output, batch["nsp_label"])
+        next_sent_loss = self.criterion(next_sent_output, batch["nsp_label"])
         mask_loss = self.criterion(mask_lm_output.transpose(1, 2), batch["mlm_labels"]) # why .transpose(1, 2)?
-        return next_loss + mask_loss
+        return next_sent_loss, mask_loss
     
-    def train(self, epoch):
-        data_iter = tqdm.tqdm(enumerate(self.train_loader), desc="EP_%s:%d" % ("train", epoch), 
-                              total=len(self.train_loader), bar_format="{l_bar}{r_bar}")
+    def run_epoch(self, epoch, is_training=True):
+        loader = self.train_loader if is_training else self.val_loader
+        mode = "train" if is_training else "validate"
+        
+        if is_training:
+            self.model.train()
+        else:
+            self.model.eval()
 
-        for idx, batch in data_iter:
-            batch = {key: value.to(self.device) for key, value in batch.items()}
-            loss = self._train_step(batch)
-            loss.backward()
+        # total_loss = 0.0
+        data_iter = tqdm.tqdm(enumerate(loader), desc=f"EP_{mode}:{epoch}", total=len(loader), bar_format="{l_bar}{r_bar}")
 
+        with torch.set_grad_enabled(is_training):
+            for idx, batch in data_iter:
+                batch = {key: value.to(self.device) for key, value in batch.items()}
+
+                next_sent_loss, mask_loss = self._train_step(batch)
+                loss = next_sent_loss + mask_loss
+
+                self.run[f"{mode}/batch_loss"].log(loss.item())
+
+                if is_training:
+                    loss.backward()
     
 
 if __name__ == '__main__':
@@ -48,7 +72,10 @@ if __name__ == '__main__':
     trainer = Trainer()
 
     for i in range(num_epoch):
-        trainer.train(i)
+        trainer.run_epoch(i)
+        # trainer.run_epoch(is_training=False)
+    
+    trainer.stop_logging()
 
  
 # train_loader = DataLoader(train_data, batch_size=2, shuffle=True, pin_memory=True)
